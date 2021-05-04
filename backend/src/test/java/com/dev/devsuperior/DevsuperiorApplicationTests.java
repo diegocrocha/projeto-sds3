a8981e4d3d392dd483a60ac5e58a4fdb7222e0ce
DIEGO CARREIA ROCHA <diego.rocha@bandtec.com.br>
2021-05-04 13:43:37 -0300

    init project

---
+++ backend/src/test/java/com/dev/devsuperior/DevsuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.devsuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
